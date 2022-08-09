f2ddbea8182bf03d644605df1f30c3345f8ee2e1
지원 <92582758+jasumine@users.noreply.github.com>
2022-08-10 00:17:16 +0900

    59, 60
    객체마다 번호를 줄려면 생성자에 id 변수를 추가한다.
    하지만 정적변수를 초기화 할려면 class 외부에서 해주어야 한다.
    static을 사용할 경우 private 변수 접근이 가능할 뿐만 아니라, 전역변수일 때와 달리 class 내부 변수로 구분이 쉬워진다.
    
    const를 사용할 경우 값을 수정할 수 없다. 오류가 생기지 않도록 const를 변수 앞에 적어준다.
    함수의 경우 const를 뒤에 적어준다.

---
+++ class59/class59/ex01.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// int idCounter = 1; // 객체마다 번호를 부여하는 일 (객체가 생기면 1씩 더해진다)
+
+class Color
+{
+public:
+	/*
+	Color() : r(0), g(0), b(0)
+	{
+		id = idCounter; // 생성자가 호출 될 때 id 의 값을 정해주고
+		idCounter++; // 다음 생성되는 객체를 위해서 ++ 해준다.
+	}
+	*/
+	// id에 idCounter++을 대입한다. 
+	Color() : r(0), g(0), b(0), id(idCounter++) { }
+
+	Color(float r, float g, float b) : r(r), g(g), b(b), id(idCounter++) { }
+
+	float getR() { return r; }
+	float getG() { return g; }
+	float getB() { return b; }
+
+	int getID() { return id; }
+
+	static Color MixColors(Color a, Color b)
+	{
+		return Color((a.r + b.r) / 2, (a.g + b.g) / 2, (a.b + b.b) / 2);
+	}
+
+	static int idCounter;
+
+private:
+	float r;
+	float g;
+	float b;
+
+	int id;
+
+};
+
+int Color::idCounter = 1; // 정적변수를 초기화 하기위해 class밖에서 정의
+
+// static을 클래스 안에 넣어두면 private함수 접근이 쉬울 뿐 아니라
+// class 안에 넣어 객체를 쉽게 구분할 수 있다.
+// 전역변수와 전역함수를 많이 만들지 않는게 좋다.
+int main()
+{
+	Color blue(0, 0, 1);
+	Color red(1, 0, 0);
+
+	Color puple = Color::MixColors(blue, red);
+
+	cout << " r = " << puple.getR() << " , g = " << puple.getG() << " , b = " << puple.getB() << endl;
+
+	cout << "red.getId() = " << red.getID() << endl;
+	cout << "blue.getId() = " << blue.getID() << endl;
+	cout << "puple.getId() = " << puple.getID() << endl;
+}
---
+++ class60/class60/ex01.cpp
@@ -0,0 +1,61 @@
+﻿// const
+// 1. 매개변수의 상수화 (모든 함수)
+// 2. 메서드의 상수화 (멤버 메서드)
+
+#include <iostream>
+
+using namespace std;
+
+class Account
+{
+public:
+	Account() : money(0) { }
+	Account(int money) : money(money) { }
+
+	void Deposit(const int d)
+	{
+		// d = money; 입금하는 금액에 오류가 생기지 않도록 const를 적어준다.
+		money += d;;
+		cout << d << "원을 예금했다!!" << endl;
+	}
+
+	void Draw(int d)
+	{
+		if (money >= d)
+		{
+			money -= d;
+			cout << d << "원을 인출했다!!" << endl;
+		}
+	}
+
+	// 상수형 메소드
+	int ViewMoney() const
+	{
+		// money++; 값을 수정하지 못함.
+		return money;
+	}
+
+
+	// 상수형 메소드가 아니지만 간혹 쓰인다.
+	const int ViewMoney()
+	{
+		return money;
+	}
+
+
+
+private:
+	int money;
+
+};
+
+
+int main()
+{
+	Account doodle(100);
+
+	doodle.Deposit(100); // 200
+	doodle.Draw(20); // 180
+
+	cout << doodle.ViewMoney() << endl;
+}
