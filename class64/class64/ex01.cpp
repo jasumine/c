719bf767c165c6be5d5a933eb3e1348fe640e771
지원 <92582758+jasumine@users.noreply.github.com>
2022-08-14 12:48:09 +0900

    64
    num(cnt++)은 num = cnt +1이여서 cnt가 먼저 num에 입력되고 cnt에 +1을 해주는것이다.
    포인터 + int는 ptr[num]이라는 뜻이다.
    cnt는 obj[5]가 생성된 만큼 더해지기 때문에 5가 된다.

---
+++ class64/class64/ex01.cpp
@@ -0,0 +1,46 @@
+﻿// 3. 코드 출력결과
+
+#include <iostream>
+
+using namespace std;
+
+class MyClass
+{
+public:
+	MyClass() : num(cnt++) , ch('\n') { } // num = cnt +1 이라서 0을 넣고 1이 더해진다.
+	
+	void Check(MyClass* ptr)
+	{
+		if (ptr + num == this) // ptr + num == ptr[num] == &obj[num]
+		{
+			cout << num << endl;
+		}
+	}
+
+	static int cnt;
+private:
+	int num;
+	char ch;
+};
+
+int MyClass::cnt = 0;
+
+int main()
+{
+	MyClass obj[5];
+
+	cout << "Test #1 : " << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		obj[i].Check(obj); // 1 2 3 4 5 -> 0 1 2 3 4 
+		// obj == &obj[0]
+	}
+
+	cout << "Test #2 : " << endl;
+	for (MyClass& i : obj) // 0부터 4 까지
+	{
+		i.Check(obj); // 1 2 3 4 5 -> 0 1 2 3 4 
+	}
+
+	cout << "cnt = " << MyClass::cnt << endl; // 10 -> 5
+}
