e4f0dcf125dd7f4b806fa2dd7e010e4a7e51a86f
지원 <92582758+jasumine@users.noreply.github.com>
2022-08-15 23:55:44 +0900

    65
    프로그램을 실행하면서 메모리낭비를 하지 않기 위해 동적할당을 사용한다.
    포인터를 이용하고 new 연산자를 붙여준다. 그리고 반드시 delete를 해주어야 한다.배열의 경우 delete[]
    객체도 똑같이 사용한다.

---
+++ class65/class65/ex01.cpp
@@ -0,0 +1,24 @@
+﻿// 정적 할당: int a;
+// 동적 할당: 프로그램 실행 중에 변수를 메모리에 할당하는 것
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// a 메모리 할당
+	int *a = new int(5); // operator new 연산자
+
+	cout << a << endl; // a의 주소값
+	cout << *a << endl; // 5
+
+	*a = 10;
+
+	cout << a << endl;
+	cout << *a << endl; // 10
+
+	// a 메모리 해제
+	delete a; 
+
+}
---
+++ class65/class65/ex02.cpp
@@ -0,0 +1,29 @@
+﻿// 배열 동적 할당
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int* arr; // &arr[0] == arr 
+	int len;
+
+	cout << "동적할당 할 배열 길이 입력 : ";
+	cin >> len;
+
+	arr = new int[len]; // len 길이의 int형 배열을 arr에 할당
+
+	for (int i = 0; i < len; i++)
+	{
+		arr[i] = len - i;
+	}
+
+	for (int i = 0; i < len; i++)
+	{
+		cout << arr[i] << endl;
+	}
+	
+	delete[] arr; // 배열 해제
+
+}
