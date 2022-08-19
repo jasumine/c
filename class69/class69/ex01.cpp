491815a7af2314b89842d17ad17a69a202035193
지원 <92582758+jasumine@users.noreply.github.com>
2022-08-20 00:16:30 +0900

    69, 70
    함수안에 객체를 선언해 값을 return 하고, main함수에서 생성한 객체에 그 함수의 값을 넣을 경우 불필요한 깊은복사를 해서 메모리가 낭비된다.
    임시객체가 깊은복사를 하지 않도록 &&r 즉 rvalue 참조자를 매개변수로 받아서 기존에 strcpy가 아닌 strData = rhs.strData를 통해 얕은 복사를 해준다. (기존에 const string &rhs에 얕은복사 값이 들어갈 수 있어서 깊은복사가 발생함.)
    얕은복사가 진행된 후 rhs.strData = nuill로 strData를 임시객체만 가르키도록 바꾸어준다.

--- class69/class69/ex01.cpp
+++ class69/class69/ex01.cpp
@@ -23,7 +23,7 @@ public:
 		strcpy(strData, str);
 	}
 
-	String(const String& rhs)
+	String(const String& rhs) // 복사 생성자
 	{
 		cout << "String(const String &) : " << this << endl;
 		len = rhs.len;
@@ -38,7 +38,7 @@ public:
 		strData = NULL;
 	}
 
-	String& operator=(const String& rhs) 
+	String& operator=(const String& rhs) // 복사 대입 연산자
 	{
 		cout << "String &operator=(const String&) : " << this << endl;
 		if (this != &rhs) 
@@ -94,4 +94,19 @@ int main()
 	cout << "===== 1 =====" << endl;
 	a = getName();
 	cout << "===== 4 =====" << endl;
+
+	/*
+	생성자가 총 4번 발생한다.
+	1. String a , 2. String res , 3. return res, 4. a = getName();
+	이 과정에서 깊은 복사는 return res에서 임시 객체로, 임시 객체에서 a 로 2번 발생한다.
+	res의 값이 저장되는 과정에서 임시 객체가 생성된다.
+	임시객체에는 복사 생성자로 깊은복사가 일어나는 불필요한 과정이 생겨
+	주소값을 받아오는 얕은 복사를 해야한다.
+	*/
+
+
+	String&& r = getName(); // rvalue참조자(&&r) 가 getName()을 참조한다.
+
+	// rvalue -> 우변에만 오는 값, 수정을 할 수 없음, **임시객체**
+	// lvalue -> 좌변, 우변 둘다 올 수 있는 값, 메모리상에 저장이 되어있는 값
 }
---
+++ class70/class70/ex01.cpp
@@ -0,0 +1,124 @@
+﻿#define _CRT_SECURE_NO_WARNINGS // strcpy함수 사용 시 경고 제거
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class String
+{
+public:
+	String()
+	{
+		cout << "String() : " << this << endl;
+		strData = NULL;
+		len = 0;
+	}
+
+	String(const char* str)
+	{
+		cout << "String(const char*) : " << endl;
+		len = strlen(str);
+		alloc(len);
+		strcpy(strData, str);
+	}
+
+	String(const String& rhs) // const가 붙어서 rvalue값도 들어간다.
+	{
+		cout << "String(const String &) : " << this << endl;
+		len = rhs.len;
+		alloc(len);
+		strcpy(strData, rhs.strData);
+	}
+
+	String(String&& rhs) // 이동 생성자, rvalue 참조를 매개변수로 받는다.
+	{
+		cout << "String(String&&) : " << this << endl;
+		len = rhs.len;
+		strData = rhs.strData; // 얕은 복사
+		rhs.strData = NULL; 
+		// &&rhs == res.strData -> "Doodle" <- 임시객체.strData 가르키는 상황이였는데
+		// null로 바꾸면 임시객체만 Doodle만 가르키는 상황이된다.
+		// delete를 해주어도 res.strData의 값은 null이기때문에 "Doodle"은 지워지지 않는다.
+	}
+
+
+	~String()
+	{
+		cout << "~String() : " << this << endl;
+		release();
+		strData = NULL;
+	}
+
+	String& operator=(const String& rhs) // 복사 대입 연산자
+	{
+		cout << "String &operator=(const String&) : " << this << endl;
+		if (this != &rhs)
+		{
+			release();
+			len = rhs.len;
+			alloc(len);
+			strcpy(strData, rhs.strData);
+		}
+		return *this;
+	}
+
+	String& operator=(String&& rhs) // 얕은 복사
+	{
+		cout << "String &operator=(String&&) : " << this << endl;
+		len = rhs.len;
+		strData = rhs.strData;
+		rhs.strData = NULL;
+		return *this; // a = b = c 때문에 사용한다.
+	}
+
+	char* getStrData() const
+	{
+		return strData;
+	}
+
+	int getLen()const
+	{
+		return len;
+	}
+
+
+private:
+	char* strData;
+	int len;
+
+	void alloc(int len)
+	{ // 깊은 복사가 일어날 경우 출력된다.
+		strData = new char[len + 1];
+		cout << "strData allocted : " << (void*)strData << endl;
+	}
+
+	void release()
+	{
+		delete[] strData;
+		if (strData) cout << "strData released : " << (void*)strData << endl;
+	}
+};
+
+String getName()
+{
+	cout << "===== 2 =====" << endl;
+	String res("Doodle");
+	cout << "===== 3 =====" << endl;
+	return res;
+}
+
+
+int main()
+{
+	String a;
+	cout << "===== 1 =====" << endl;
+	a = getName();
+	cout << "===== 4 =====" << endl;
+	/*
+	# a=임시객체(rvalue) 
+	임시객체는 rvalue 인데 &&rhs를 생성해주기 전에는 
+	const &rhs에도 rvalue가 들어갈 수 있어서 깊은 복사가 발생했다.
+	&&rhs를 이동생성자를 통해 얕은 복사로 필요없는 메모리 할당을 하지 않아도 된다.
+	*/
+}
